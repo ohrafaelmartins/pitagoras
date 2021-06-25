dd5181a3d7c3b27ba2957f907c9047f575314d94
Rafael Martins <rafaelmartins025@outlook.com>
2021-06-25 15:11:46 -0300

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,28 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "pitagoras"
+  user_data     = file("install-docker")
+
+  tags = {
+    Name = "pitagoras"
+  }
+
+  vpc_security_group_ids = [
+    aws_security_group.allow-ssh.id,
+    aws_security_group.allow-http.id,
+    aws_security_group.allow-https.id
+  ]
+}
